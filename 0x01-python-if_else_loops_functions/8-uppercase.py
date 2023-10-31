579f8d7684381d794a159500e087a6473da6523f
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-01 00:37:51 +0100

    print uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = " "
     for i in str:
         if ord('a') <= ord(i) <= ord('z'):
-            upper = chr(ord(i) - (ord('a') - ord('A')))
+            i = chr(ord(i) - (ord('a') - ord('A')))
             result += upper
-        else:
-            result += i
-    print("{}".format(result), end="")
-    print(end="")
+    print("{}".format(i), end="")
+    print()
 

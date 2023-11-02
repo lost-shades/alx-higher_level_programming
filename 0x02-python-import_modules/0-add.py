ec08ad521901eb82cb7a5e923b28d1e190df6441
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-02 12:51:42 +0100

    calculation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     b = 2
 
     from add_0 import add
-
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result =  add(a, b)
+    print("{} + {} = {}".format(a, b, result))

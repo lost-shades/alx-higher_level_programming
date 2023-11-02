9fd9c58ef04770810e5b32a8ce52a55a3e8b2941
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-02 14:57:50 +0100

    system args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    num_arguments = len(sys.argv) - 1 
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+        print("1:", sys.argv[1])
+    else:
+        print("{} arguments:".format(num_arguments))
+        for i in range(1, num_arguments + 1):
+            print(f"{i}:", sys.argv[i])

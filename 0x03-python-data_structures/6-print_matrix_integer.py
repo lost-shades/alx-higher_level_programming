c0eb9df139a26ef8a7e2668cc374eb24d29b361d
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-04 17:42:39 +0100

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for elements in row:
-            print("{:d}".format(elements), end=" " 
-            if elements != row[len(row)-1]):
+            print("{:d}".format(elements), end="")
+            if (elements != row[len(row)-1]):
                 print(" ", end="")
         print()

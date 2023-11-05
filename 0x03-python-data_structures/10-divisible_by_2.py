0746e4e11b85dc56b4cf2a32fe1c8943df0fe3ea
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-05 19:37:07 +0100

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 def divisible_by_2(my_list=[]):
     result = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             result.append(True)
         else:
             result.append(False)

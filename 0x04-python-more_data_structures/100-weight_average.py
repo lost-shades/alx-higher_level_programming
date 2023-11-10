7de9eafcfc0c4f49396cbfcfed3ef7566685dd6c
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-10 15:18:53 +0100

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        total_score = 0
+        total_weight = 0
+
+        for score, weight in my_list:
+        
+            total_score += score * weight
+            total_weight += weight
+
+        if total_weight == 0:
+            return 0  # Avoid division by zero
+        else:
+            return total_score / total_weight

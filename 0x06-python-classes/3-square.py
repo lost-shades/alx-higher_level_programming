1e3e898594eb8179089d4b8330c5901ec5688678
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-22 19:20:09 +0100

    3 square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""provides functionality related to square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """
+        Initializes a new square.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2

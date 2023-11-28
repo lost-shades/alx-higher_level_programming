9fcb2c9e812f548b332f375b035a346f8231f64c
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-28 14:01:02 +0100

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -52,4 +52,8 @@ if __name__ == "__main__":
         print("N must be a number")
         sys.exit(1)
 
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
     solve_nqueens(N)

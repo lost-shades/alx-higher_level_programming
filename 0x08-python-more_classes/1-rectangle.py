ecc1cc5e1819bbc6ad26d1574a88a71b811ac381
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-28 11:00:01 +0100

    next

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width
@@ -20,7 +20,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
 
     @property
     def height(self):

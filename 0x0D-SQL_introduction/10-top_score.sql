e73c8a08286bc3bcd4f0cd9abdb455629e797ace
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2024-06-12 01:03:13 +0100

    mysql tasks

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists all record of the table in a particular order
-SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,9 @@ CREATE TABLE second_table (
 	score INT
 );
 INSERT INTO second_table (id, name, score) 
-VALUES (1, 'John', 10), 
+VALUES (
+	(1, 'John', 10), 
 	(2, 'Alex', 3), 
 	(3, 'Bob', 14), 
-	(4, 'George', 8);
+	(4, 'George', 8)
+);

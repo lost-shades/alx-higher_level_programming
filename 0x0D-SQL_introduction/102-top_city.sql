ce992aa096201da7a88994dcb12be3f1dc55a272
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2024-06-13 04:14:01 +0100

    mysql advance task

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- script that displays the top 3 cities temperature in July and Aug
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3;

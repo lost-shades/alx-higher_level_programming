a172fcf9c94ddf75959b177cf8cb4661dbd197ac
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2024-06-13 02:25:50 +0100

    mysql tasks

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,3 @@
 -- converts database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table 
-	MODIFY id int(11) DEFAULT NULL,
-	MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;

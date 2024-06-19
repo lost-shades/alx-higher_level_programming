4cf2c4667cc499c1f33a717d7e570d78570aec90
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2024-06-19 03:43:40 +0100

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
--- Script that lists all cities contained in the database hbtn_0d_usa
-SELECT c.id, c.name, s.name FROM cities c INNER JOIN states s ON c.state_id = s.id;
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT s.title, g.genre_id FROM tv_shows s INNER JOIN tv_show_genres g ON s.id = g.show_id ORDER BY s.title, g.genre_id;
